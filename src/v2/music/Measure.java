8bd4350436a3d38e7ada842138e901ef4c02c490
judithhinlung <judithhin@gmail.com>
2020-10-03 19:06:52 -0700

    Created MeasureElement interface

--- src/v2/music/Measure.java
+++ src/v2/music/Measure.java
@@ -1,19 +1,40 @@
 package v2.music;
 import java.util.ArrayList;
+import freedots.math.Fraction;
 public class Measure {
+  Part part;
   private int number;
-  ArrayList<Staff> staves = new ArrayList<Staff>();
   KeySignature keySignature = null;
   TimeSignature timeSignature = null;
-  public Measure(int number) {
+  Fraction offset = Fraction.ZERO;
+  ArrayList<MeasureElement> elements = new ArrayList<MeasureElement>();
+  public Measure(Part part, int number) {
+    this.part = part;
     this.number = number;
-    staves.add(new Staff(1));
   }
-  public Measure(int number, int numStaves) {
-    this.number = number;
-    for (int i = 1; i <= numStaves; i++) {
-      staves.add(new Staff(i));
+  public Part getPart() {
+    return this.part;
+  }
+  public int getNumber() {
+    return this.number;
+  }
+  public KeySignature getKey() {
+    if (this.keySignature == null) {
+      return part.getCurrentKey(this.number);
     }
+    return this.keySignature;
+  }
+  public void setKey(KeySignature key) {
+    this.keySignature = key;
+  }
+  public TimeSignature getTime() {
+    if (this.timeSignature == null) {
+      return part.getCurrentTime(this.number);
+    }
+    return this.timeSignature;
+  }
+  public void setTime(TimeSignature time) {
+    this.timeSignature = time;
   }
   private boolean printMeasureNumber = true;
   public void setPrintMeasureNumber(boolean printMeasureNumber) {
@@ -23,5 +44,11 @@ public class Measure {
   public void setWholeMeasureRest(boolean wholeMeasureRest) {
     this.isWholeMeasureRest = wholeMeasureRest;
   }
-  // TODO: Create a Event class to hold music elements in time
+  public Fraction getOffset() {
+    return this.offset;
+  }
+  public void addElement(MeasureElement element) {
+    elements.add(element);
+    offset.add(element.getMoment());
+               }
 }
--- src/v2/music/MeasureElement.java
+++ src/v2/music/MeasureElement.java
@@ -1,4 +1,8 @@
 package v2.music;
+import freedots.math.Fraction;
 public interface MeasureElement {
   public Measure getMeasure();
+  public Fraction getMoment();
+  public int getStaff();
+  public int getVoice();
 }
